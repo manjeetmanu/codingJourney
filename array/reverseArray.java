dee6782968390e04bd2ddc418d7fa79866de49c6
Manjeet <manjeetmanu12061998@gmail..com>
2022-07-25 09:16:45 +0530

    Reverse Array

---
+++ array/reverseArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class reverseArray {
+	public static void main (String[] args) {
+		//code
+		Scanner sc=new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0){
+		    int n = sc.nextInt();
+		    int[] arr = new int[n];
+		    for(int i=0;i<n;i++){
+		        arr[i]=sc.nextInt();
+		    }
+		    
+		    for(int i=0;i<n/2;i++){
+		        int temp=arr[i];
+		        arr[i]=arr[n-i-1];
+		        arr[n-i-1]=temp;
+		    }
+		    for(int i=0;i<n;i++){
+		        System.out.print(arr[i]+" ");
+		    }
+		    System.out.println();
+		    t--;
+		}
+		
+	}
+}
